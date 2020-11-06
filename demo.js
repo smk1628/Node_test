47365ca0e1c34707ad104a3d02f3f8694dfbb753
smk1628 <1628150422@qq.com>
2020-11-06 15:31:17 +0800

    creat demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,6 @@
-function demo(){
-    console.log(arguments.callee);
-}
-demo()
+/**
+ * 输出node的外层函数 function (exports, require, module, __filename, __dirname) {}
+ * */
+
+
+console.log(arguments.callee.toString()); //输出外层函数
