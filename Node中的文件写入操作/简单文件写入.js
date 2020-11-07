2880056406a7111a4f0772385258c76cb496c0cb
smk1628 <1628150422@qq.com>
2020-11-07 18:00:44 +0800

    updata

---
+++ Node中的文件写入操作/简单文件写入.js
@@ -0,0 +1,23 @@
+/**
+ * 1.异步文件写入
+ *      fs.writeFile(file,data[,options],callback)
+ *              --file 要写入的文件路径+文件名+后缀
+ *              --data 要写入的数据
+ *              --options 可选参数
+ *              --callback 回掉函数
+ *                  --err 错误对象
+ *  在Node中有这样一个原则：错误优先
+ * */
+
+//引入内置的fs模块
+let fs = require('fs')
+
+//调用writeFile方法
+fs.writeFile('./demo.txt','hello node',(err)=>{
+    if(err){
+        console.log('文件写入失败',err);
+    }else{
+
+        console.log('文件写入成功！');
+    }
+})
