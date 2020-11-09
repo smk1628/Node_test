d37259b3014f1d345c64976459f75fad71591b7a
smk1628 <1628150422@qq.com>
2020-11-09 19:55:25 +0800

    updata

--- Express服务器/express.js
+++ Express服务器/express.js
@@ -7,11 +7,17 @@ app.get('/haha',function (request,response){
     response.send("it's haha")
 })
 app.get('/',function (request,response){
+    /**
+     * 什么样的请求，能交给这个回调函数处理？
+     * 1.请求方式必须为GET
+     * 2.请求的URI必须为"/"
+     * PS：路由可以理解为一组组key-value组合，key：请求方式+URI路径，value：后端所处理该请求的回调函数
+     * */
     response.send("it's index")
 })
 
 //指定服务器运行端口（绑定监听端口）
 app.listen(3000,function (err){
     if(!err) console.log('server success')
-    else console.log(err)
+
 })
