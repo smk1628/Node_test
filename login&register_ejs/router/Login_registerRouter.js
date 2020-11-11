70c8ff12d09f5053e5f372e4dea801d333cf654d
smk1628 <1628150422@qq.com>
2020-11-11 18:56:15 +0800

    update README.md

--- login&register_ejs/router/Login_registerRouter.js
+++ login&register_ejs/router/Login_registerRouter.js
@@ -19,18 +19,18 @@ router.post('/login',(request,response)=>{
     //获取用户输入
     const {email,password} = request.body
     //收集错误信息
-    let errMsg = {}
+    let userMsg = {}
     //console.log(email,password)
     if(!emailReg.test(email)){
         //response.send('您的邮箱格式不正确，请重新输入！')
-        errMsg.emailErr = '您的邮箱格式不正确，请重新输入！'
+        userMsg.emailErr = '您的邮箱格式不正确，请重新输入！'
     }
     if(!passwordReg.test(password)){
         //response.send('您的密码格式不正确，请重新输入！')
-        errMsg.passwordErr = '您的密码格式不正确，请重新输入！'
+        userMsg.passwordErr = '您的密码格式不正确，请重新输入！'
     }
-    if(JSON.stringify(errMsg) !== '{}'){
-        response.render('login',{errMsg})
+    if(JSON.stringify(userMsg) !== '{}'){
+        response.render('login',{userMsg})
         return
     }
     usersModel.findOne({email,password},function (err,data){
@@ -38,12 +38,12 @@ router.post('/login',(request,response)=>{
             response.send(`欢迎${data.name}登录`)
         }else if(err){
             //response.send('网络不稳定，请稍后再试！')
-            errMsg.netErr = '当前网络不稳定，请稍后再试！'
-            response.render('login',{errMsg})
+            userMsg.netErr = '当前网络不稳定，请稍后再试！'
+            response.render('login',{userMsg})
         }else {
             //response.send('用户名或密码错误，请重新输入')
-            errMsg.loginErr = '用户名或密码错误，请重新输入'
-            response.render('login',{errMsg})
+            userMsg.loginErr = '用户名或密码错误，请重新输入'
+            response.render('login',{userMsg})
         }
     })
 })
@@ -59,47 +59,47 @@ router.post('/register',(request,response)=>{
     //获取用户输入
     const {email,name,password,re_password} = request.body
     //收集错误信息
-    let errMsg = {}
+    let userMsg = {}
     //使用正则去校验
     if(!emailReg.test(email)){
         //response.send('邮箱不合法')
-        errMsg.emailErr = '邮箱不合法'
+        userMsg.emailErr = '邮箱不合法'
     }
     if(!nameReg.test(name)){
         //response.send('昵称不合法，必须为中文')
-        errMsg.nameErr = '昵称不合法，必须为中文'
+        userMsg.nameErr = '昵称不合法，必须为中文'
     }
     if(!passwordReg.test(password)){
         //response.send('密码不合法，必须6-20位')
-        errMsg.passwordErr = '密码不合法，必须6-20位'
+        userMsg.passwordErr = '密码不合法，必须6-20位'
     }
     if(password !== re_password){
         //response.send('两次密码不一致')
-        errMsg.re_passwordErr = '两次密码不一致'
+        userMsg.re_passwordErr = '两次密码不一致'
     }
-    if(JSON.stringify(errMsg) !== '{}'){
-        //console.log(errMsg)
-        response.render('register',{errMsg})
+    if(JSON.stringify(userMsg) !== '{}'){
+        //console.log(userMsg)
+        response.render('register',{userMsg})
         return
     }
     //去数据库中查询邮箱是否注册过
     usersModel.findOne({email},function (err,data){
         if(data){
             //response.send('该邮箱已注册！')
-            errMsg.emailErr = '该邮箱已注册！'
-            response.render('register',{errMsg})
+            userMsg.emailErr = '该邮箱已注册！'
+            response.render('register',{userMsg})
             return
         }
         usersModel.create({email,name,password},function (err,data){
             if(!err){
                 console.log(`邮箱为${email}的用户注册成功！`)
                 //response.redirect('/login')
-                response.render('login',{errMsg:{email,password}})
+                response.render('login',{userMsg:{email,password}})
             }
             else{
                 //response.send('注册失败，稍后重试！')
-                errMsg.netErr = '当前网络不稳定，稍后重试！'
-                response.render('register',{errMsg})
+                userMsg.netErr = '当前网络不稳定，稍后重试！'
+                response.render('register',{userMsg})
             }
         })
     })
