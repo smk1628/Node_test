f80be580632e4a92a02a7401d67d683d7cceb3fc
smk1628 <1628150422@qq.com>
2020-11-16 17:26:21 +0800

    update

---
+++ login&register_cookie/router/UIRouter.js
@@ -0,0 +1,49 @@
+/**
+ * 专门用来管理UI页面的路由
+ * */
+//Router 是一个完整的中间件和路由系统，也可以看成是一个小型的app对象
+//引入Router构造函数
+const {Router} = require('express')
+//引入cookie-parser
+const cookieParser = require('cookie-parser')
+//引入数据模型
+const userModel = require('../models/usersModel')
+//创建一个Router实例
+const router = new Router()
+//使用cookie-parser
+router.use(cookieParser())
+
+//引入path模块，Node内置的专门解决UI路由路径问题的库
+let {resolve} = require('path')
+//重定向根路由
+router.get('/',(request,response)=>{
+    response.redirect('https://www.baidu.com/')
+})
+//展示登录页面路由 ---UI路由
+router.get('/login',(request,response)=>{
+    let url = resolve(__dirname,'../public/login.html')
+    response.sendFile(url)
+})
+//展示注册页面路由 ---UI路由
+router.get('/register',(request,response)=>{
+    let url = resolve(__dirname,'../public/register.html')
+    response.sendFile(url)
+})
+//展示用户中心页面路由 ---UI路由
+router.get('/user_center',(request,response)=>{
+    const {_id} = request.cookies
+    if(_id){
+        //response.render('user_center',{user_name})
+        userModel.findOne({_id:_id},function (err,data){
+            if(!err && data){
+                response.render('user_center',{user_name:data.name})
+            }else response.redirect('http://localhost:3000/login')
+        })
+    }else{
+        response.redirect('http://localhost:3000/login')
+    }
+})
+
+module.exports = function (){
+    return router    //为了迎合中间件理念，中间件应该是一个函数
+}
